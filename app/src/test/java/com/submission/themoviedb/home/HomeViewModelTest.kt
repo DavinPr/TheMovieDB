b436d1debb45c0d86b73817088d8dbf62ed25fae
DavinPr <davin.game02@gmail.com>
2020-12-24 13:19:55 +0700

    perubahan AppExecutors.kt

--- app/src/test/java/com/submission/themoviedb/home/HomeViewModelTest.kt
+++
@@ -1,8 +0,0 @@
-package com.submission.themoviedb.home
-
-import org.koin.android.viewmodel.compat.ViewModelCompat.viewModel
-import org.koin.android.viewmodel.ext.android.viewModel
-
-class HomeViewModelTest {
-
-}
