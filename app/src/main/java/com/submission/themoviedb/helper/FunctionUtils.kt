2451eaabd8a35370a1df41f8a97444047fabf084
DavinPr <davin.game02@gmail.com>
2020-12-30 20:08:37 +0700

    Fix status bar

--- app/src/main/java/com/submission/themoviedb/helper/FunctionUtils.kt
+++ app/src/main/java/com/submission/themoviedb/helper/FunctionUtils.kt
@@ -1,6 +1,7 @@
 package com.submission.themoviedb.helper
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Build
 import android.view.*
 import android.view.WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
@@ -17,7 +18,7 @@ fun barSetup(window: Window, context: Context, hide: Boolean) {
                 setDecorFitsSystemWindows(false)
                 window.insetsController?.let {
                     if (hide) {
-                        it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                        it.hide(WindowInsets.Type.navigationBars())
                         it.systemBarsBehavior =
                             WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
                     } else {
@@ -31,12 +32,20 @@ fun barSetup(window: Window, context: Context, hide: Boolean) {
             }
             Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> {
                 if (hide) {
-                    window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
-                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+//                    window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+//                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+//                            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+                    window.apply {
+                        clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+                        addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+                        statusBarColor = Color.TRANSPARENT
+                    }
                 } else {
                     window.decorView.systemUiVisibility =
                         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
